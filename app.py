371cbab67fa014298cb78a76917f0407ae4d7e70
pranay-121 <157474363+pranay-121@users.noreply.github.com>
2025-04-28 20:31:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,14 +57,14 @@ input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis
 # Scale the input data
 input_data_scaled = scaler.transform(input_data)
 
-
-# Predict churn
-prediction = model.predict(input_data_scaled)
-prediction_proba = prediction[0][0]
-
-st.write(f'Churn Probability: {prediction_proba:.2f}')
-
-if prediction_proba > 0.5:
-    st.write('The customer is likely to churn.')
-else:
-    st.write('The customer is not likely to churn.')
+if(st.button("Predict Churn Probability ")):
+     # Predict churn
+    prediction = model.predict(input_data_scaled)
+    prediction_proba = prediction[0][0]
+    
+    st.write(f'Churn Probability: {prediction_proba:.2f}')
+    
+    if prediction_proba > 0.5:
+        st.write('The customer is likely to churn.')
+    else:
+        st.write('The customer is not likely to churn.')
